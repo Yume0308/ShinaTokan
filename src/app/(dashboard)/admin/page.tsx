c1c5a69f7ccf6bfc2b9bc3fbc2b2520dfc9e92e0
Pham Tuan <23020704@vnu.edu.vn>
2024-10-08 00:07:40 +0700

    Add
    UI Calendar for dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,40 @@
-const AdminPage = () => {
-  return (
-    <div>
+'use client'
 
+import { Calendar } from "@/components/ui/calendar";
+import InfoCard from "@/components/ui/infoCard"
+
+import React from "react";
+
+const AdminPage = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left  */}
+      <div className="w-full lg:w-3/4 flex flex-col gap-8">
+        {/* User card  */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <InfoCard type="student" />
+          <InfoCard type="teacher" />
+          <InfoCard type="parent" />
+          <InfoCard type="staff" />
+        </div>
+
+        {/* Middle */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full lg:w-1/4 flex flex-col gap-8 max-md:hidden">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+
+        />
+      </div>
     </div>
   );
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -71,7 +71,7 @@ export default function DashboardLayout({
       <div className="flex flex-col">
         {/* header */}
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-
+          {/* button sheet  */}
           <ButtonSheet side="left" logo={
             {
               imgSrc: "/logo.png",
@@ -81,27 +81,13 @@ export default function DashboardLayout({
           } />
           <ButtonSearch />
 
+          {/* avatar header */}
           <AvatarHeader avatarImgSrc="https://github.com/shadcn.png" avatarImgAlt="" avatarFallback="SC" nickname="Shadcn" email="Shadcn@gmail.com"/>
         </header>
 
         {/* main */}
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Books</h1>
-          </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a book.
-              </p>
-              <Button className="mt-4">Add Book</Button>
-            </div>
-          </div>
+        <main>
+          {children}
         </main>
       </div>
     </div>
